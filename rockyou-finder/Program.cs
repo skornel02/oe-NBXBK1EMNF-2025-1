286ccec520bd4335f523cfc47c9d70f8b44f3908
Stefán Kornél <stefankornel02@gmail.com>
2025-11-27 13:19:53 +0100

    RockYou finder with sha256

---
+++ rockyou-finder/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+using System.Security.Cryptography;
+
+Console.WriteLine("Hello, World!");
+
+var file = "rockyou.txt";
+
+var passwordToMatch = "fuckmedaddy123";
+var passwordToMatchHash = SHA256.HashData(System.Text.Encoding.UTF8.GetBytes(passwordToMatch));
+
+var timer = new Stopwatch();
+timer.Start();
+
+using var reader = new StreamReader(file);
+string? line;
+while ((line = reader.ReadLine()) != null)
+{
+    var hash = SHA256.HashData(System.Text.Encoding.UTF8.GetBytes(line));
+    if (Enumerable.SequenceEqual(passwordToMatchHash, hash))
+    {
+        Console.WriteLine($"Password found: {line}");
+        return;
+    }
+}
+
+Console.WriteLine("Password not found.");
+timer.Stop();
+
+var time = timer.ElapsedMilliseconds;
+Console.WriteLine($"Time taken: {time}");
